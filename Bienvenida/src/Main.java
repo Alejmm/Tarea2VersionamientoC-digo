c6f2ec7bd163231ac4207f01bd1ff92ace365a32
Alejandro <cristianmelgar1010@gmail.com>
2022-07-22 00:15:19 -0600

    Proyecto Original

---
+++ Bienvenida/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+
+        Scanner entrada = new Scanner(System.in);
+        String nombre    = entrada.next();
+        System.out.println("Bienvenido a Progra  2 2022  "+ nombre);
+    }
+}
